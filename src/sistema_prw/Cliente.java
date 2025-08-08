dded54f4f5207a86c146ec62f988745c6ec9780b
Antonio-Farias-Neto <antoniofarias1880@gmail.com>
2025-08-07 23:31:24 -0300

    Adicionando comentários a classe Cliente

--- src/sistema_prw/Cliente.java
+++ src/sistema_prw/Cliente.java
@@ -2,11 +2,20 @@ package sistema_prw;
 
 import java.util.Objects;
 
+/**
+ * Representação de um cliente , o qual possui nome, email e um prato preferido.
+ * @author Antonio Neto
+ */
 public class Cliente {
 	private String nome;
 	private String email;
 	private String pratoPreferido;
 	
+	/**
+	 * constroi um cliente que inicia sem um prato preferido definido, pois ainda não provou nenhum
+	 * @param nome é o nome do cliente
+	 * @param email é o email do cliente
+	 */
 	public Cliente(String nome, String email) {
 		this.nome = nome;
 		this.email = email;
